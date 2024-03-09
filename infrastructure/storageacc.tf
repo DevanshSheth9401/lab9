c1a1f9ce234b8856364a22fa9381969ce33154b8
DevanshSheth9401 <shet0028@algonquinlive.com>
2024-03-08 23:51:04 -0500

    Commit 1

---
+++ infrastructure/storageacc.tf
@@ -0,0 +1,11 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "teststorage9401"
+  resource_group_name      = "IAC"
+  location                 = "canada central"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
